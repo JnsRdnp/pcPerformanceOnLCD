1c7765930f95eb812a64a754a2bf4c72b5032c5b
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2023-03-15 21:20:31 +0200

    Added  cycling between hardware info  and weather+time

--- main.py
+++ main.py
@@ -3,9 +3,33 @@ import psutil
 import GPUtil
 import time
 import datetime
+from requests_html import HTMLSession
 
 arduinoData = serial.Serial('com3',115200)
-t=0
+
+stateHandle = 0
+
+hwInfoPartOutOfMax = 7
+maxNum = 10
+
+#weather
+s = HTMLSession()
+
+
+def getTempertature(city):
+    query = city
+    url = f'https://www.google.com/search?q={query}+tempertature'
+
+
+    r = s.get(url,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'})
+
+    #print(r.html.find('title',first = True).text)
+    title_text = r.html.find('title', first=True).text
+
+    weatheroutput = "Oulu: "+str(r.html.find('span#wob_tm',first = True).text)
+    #print(myCmd)
+    return weatheroutput
+
 
 def get_gpu_temperature():
     gpus = GPUtil.getGPUs()
@@ -22,20 +46,27 @@ def get_gpu_info():
     return None
 
 while True:
+    if stateHandle<=hwInfoPartOutOfMax:
+        formatted_numCPU = f"{(psutil.cpu_percent(interval=1)):.1f}".zfill(4) 
+        sysInfo=(str(formatted_numCPU) + "%|" + str(get_gpu_info())+"%|"+str(get_gpu_temperature()))
+    elif stateHandle>hwInfoPartOutOfMax:
+        currentTime = (datetime.datetime.now().strftime("%H:%M"))
+        currentTimeStr = str(currentTime)
+        sysInfo=currentTimeStr+getTempertature("Oulu")
+        if stateHandle==maxNum:
+            stateHandle=0
 
-    formatted_numCPU = f"{(psutil.cpu_percent(interval=1)):.1f}".zfill(4) 
-    sysInfo=(str(formatted_numCPU) + "%|" + str(get_gpu_info())+"%| "+str(get_gpu_temperature()))
     cmd=sysInfo
     cmd=cmd+'\r'
 
     arduinoData.write(cmd.encode())
 
-    time.sleep(2.5)
+    stateHandle+=1
+    time.sleep(3)
 
-    current_time = datetime.datetime.now().strftime("%H:%M")
-    current_timeStr = str(current_time)
-    arduinoData.write(current_timeStr.encode())
-    time.sleep(2.5)
 
+
+
+    
 
     
