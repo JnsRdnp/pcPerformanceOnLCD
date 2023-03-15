24fe464a127d4cdbb0f4e99b2f96bc41f232ff79
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2023-03-15 02:41:55 +0200

    added time that appears on gpu text

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import serial
 import psutil
 import GPUtil
 import time
+import datetime
 
 arduinoData = serial.Serial('com3',115200)
 t=0
@@ -23,9 +24,18 @@ def get_gpu_info():
 while True:
 
     formatted_numCPU = f"{(psutil.cpu_percent(interval=1)):.1f}".zfill(4) 
-    sysInfo=(str(formatted_numCPU) + "%|" + str(get_gpu_info())+"%|"+str(get_gpu_temperature()))
+    sysInfo=(str(formatted_numCPU) + "%|" + str(get_gpu_info())+"%| "+str(get_gpu_temperature()))
     cmd=sysInfo
     cmd=cmd+'\r'
+
     arduinoData.write(cmd.encode())
-    time.sleep(3)
+
+    time.sleep(2.5)
+
+    current_time = datetime.datetime.now().strftime("%H:%M")
+    current_timeStr = str(current_time)
+    arduinoData.write(current_timeStr.encode())
+    time.sleep(2.5)
+
+
     
