de0077332f411ba742cb803d779e101f12f227b1
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2023-03-15 02:42:41 +0200

    Arduino now know if it is getting time or hardware info

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -1,35 +1,64 @@
 String myCmd;
+String myTime;
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,16,2); 
 //0x27
 int a=0;
+int stateOfInput = 0;
 void setup() {
-
+  // put your setup code here, to run once:
   Serial.begin(115200);
 
   lcd.init();
   lcd.clear();         
-  lcd.backlight();      
+  lcd.backlight();      // Make sure backlight is on
   
-
-  lcd.setCursor(0,0);   
+  // Print a message on both lines of the LCD.
+  lcd.setCursor(0,0);   //Set cursor to character 2 on line 0
   lcd.print("Booting up...");
 
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
 
-  myCmd=Serial.readStringUntil('\r');
 
+  switch (stateOfInput){
+    case 0:
+      myCmd=Serial.readStringUntil('\r');
+      break;
+
+    default:
+      myTime=Serial.readStringUntil('\r');
+      break;
+
+  }
+ 
   while(Serial.available()==0){
 
   }
-  lcd.clear();
-  lcd.setCursor(1,1);
-  lcd.print(myCmd+((char)223));
-  lcd.setCursor(0,0);
-  lcd.print(" CPU | GPU | TMP");
-  delay(500);
+
+
+  switch (stateOfInput) {
+    case 0:
+        lcd.clear();
+        stateOfInput = 1;
+        lcd.setCursor(0,1);
+        lcd.print(myCmd+((char)223));
+        lcd.setCursor(0,0);
+        lcd.print(" CPU | GPU | TMP");
+        delay(100);
+        break;
+    default:
+        lcd.setCursor(0,1);
+        lcd.print(myCmd+((char)223));
+        lcd.setCursor(6,0);
+        lcd.print(myTime);
+        delay(100);
+        stateOfInput = 0;
+        break;
+  }
+
 
 
 }
